f5a46b48e59fd8bd04c8ba728e9b193292c55f4b
ivans <ivans@varyence.com>
2024-01-27 10:58:19 +0200

    Add header, total price, company and logo

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ for filepath in filepaths:
     columns = df.columns
     columns = [item.replace("_", " ").title() for item in columns]
     pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
     pdf.cell(w=40, h=8, txt=columns[0], border=1)
     pdf.cell(w=70, h=8, txt=columns[1], border=1)
     pdf.cell(w=40, h=8, txt=columns[2], border=1)
@@ -37,5 +38,22 @@ for filepath in filepaths:
         pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
         pdf.cell(w=40, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=40, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=40, h=8, txt=f"The total price is: {total_sum}", ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family="Times", size=12, style="B")
+    pdf.cell(w=50, h=8, txt=f"Python invoices generator")
+    pdf.image("logo.png", w=5)
 
     pdf.output(f"PDFs/{filename}.pdf")
